79d9ca22054528d5a00fded02926e69931f8c8f1
Bliped-Pixel <muntasirthameem.com>
2024-03-21 21:07:15 +0530

    IncidentService without generateReport function

--- src/com/service/IncidentService.java
+++ src/com/service/IncidentService.java
@@ -3,9 +3,11 @@ package com.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Date;
 
 import com.dao.IncidentDao;
 import com.dao.IncidentDaoImpl;
+import com.enums.IncidentType;
 import com.model.Incident;
 
 public class IncidentService {
@@ -15,6 +17,21 @@ public class IncidentService {
 		IncidentDao dao=new IncidentDaoImpl();
 		
 		return dao.getAllIncidents();
+	}
+	public void createIncident(Incident i) throws ClassNotFoundException, SQLException {
+		IncidentDao dao = new IncidentDaoImpl();
+		dao.createIncident(i);
+		return;
+	}
+	public List<Incident> getIncidentsInDateRange(Date from, Date to) throws ClassNotFoundException, SQLException
+	{
+		IncidentDao dao = new IncidentDaoImpl();
+		return dao.getIncidentsInDateRange(from, to);
+	}
+	public List<Incident> searchIncidents(IncidentType criteria)throws ClassNotFoundException, SQLException
+	{
+		IncidentDao dao = new IncidentDaoImpl();
+		return dao.searchIncidents(criteria);
 	}
 
 }
