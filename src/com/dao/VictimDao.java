987ff01656baff41cf880fea9f9bfb44c6ba7831
Nandhini <114504447+20tuit103@users.noreply.github.com>
2024-03-27 11:22:26 +0530

    Update VictimDao.java

--- src/com/dao/VictimDao.java
+++ src/com/dao/VictimDao.java
@@ -4,11 +4,11 @@ import java.sql.SQLException;
 
 import java.util.List;
 
+import com.exceptions.InvalidIncidentDataException;
 import com.model.Victim;
 
 public interface VictimDao {
-	public void addRecord( String firstName, String lastName, String dob, String gender, String contactInfo,
-			int incidentId) throws ClassNotFoundException, SQLException;
+	public void addRecord( Victim victim) throws ClassNotFoundException, SQLException,InvalidIncidentDataException;
 	public List<Victim> getVictim(int id1) throws ClassNotFoundException, SQLException ;
 	public List<Victim> getVictimByName(String name) throws ClassNotFoundException, SQLException;
 
